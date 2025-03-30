20d84af1ed12e3ec36f70e6afa00fc0d9eb3b8f7
AdiSuRpRiSe <adam@kamos.eu>
2025-03-30 16:01:40 +0000

    Deploying to gh-pages from @ AdiSuRpRiSe/adisurprise.github.io@97fc770d5ce36d010863d39f86c5ab470698f9de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,20 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-narzędzia",
-          title: "Narzędzia",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-biblioteczka",
           title: "Biblioteczka",
           description: "",
